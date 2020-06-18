b246c905f74c96e82839b7abb4a8524ae5a23bb5
tomasz wyrwa <wyrwa.t@gmail.com>
2020-06-18 19:54:07 +0200

    add resetRound()

--- js/script.js
+++ js/script.js
@@ -1,31 +1,30 @@
-const gameSummary = {
+let gameSummary = {
   roundsNumber: 0,
   playerScore: 0,
   aiScore: 0,
   ties: 0,
 };
 
-const game = {
+let game = {
   playerHand: "",
   aiHand: "",
 };
 
 const hands = document.querySelectorAll(".select img");
-
-
+const restartBtn = document.querySelector(".restart-btn");
+const showPlayerChoice = document.querySelector(".player-choice");
+const showAiChoice = document.querySelector(".ai-choice");
+const roundResult = document.querySelector(".round-score");
 
 function compChoice() {
   const aiHand = hands[Math.floor(Math.random() * 3)];
   const aiHandTemp = aiHand.cloneNode(true);
-  const showAiChoice = document.querySelector(".ai-choice");
   showAiChoice.innerHTML = "";
   showAiChoice.appendChild(aiHandTemp);
   game.aiHand = aiHandTemp.dataset.option;
 }
 
 function roundScore() {
-  let i = 0;
-  const roundScore = document.querySelector(".round-score");
   const score = document.createElement("div");
   const roundCounter = document.createElement("p");
   const scoreElement = document.createElement("p");
@@ -34,36 +33,35 @@ function roundScore() {
   scoreElement.innerHTML = `${game.playerHand} : ${game.aiHand}`;
   score.appendChild(roundCounter);
   score.appendChild(scoreElement);
-  roundScore.appendChild(score);
-  roundScore.insertBefore(score, roundScore.firstChild);
+  roundResult.appendChild(score);
+  roundResult.insertBefore(score, roundResult.firstChild);
 }
 
 function checkResult(player, ai) {
   if (player === ai) {
-    return 'draw';
-  } else if ((player === 'paper' && ai === 'rock') || (player === 'rock' && ai === 'scissors') || (player === 'scissors' && ai === 'paper')) {
-    return 'win';
-  } else return 'loss';
+    return "draw";
+  } else if ((player === "paper" && ai === "rock") || (player === "rock" && ai === "scissors") || (player === "scissors" && ai === "paper")) {
+    return "win";
+  } else return "loss";
 }
 
 function publishResult(player, ai, result) {
   gameSummary.numbers++;
-  document.querySelector('p.rounds-number span').textContent = gameSummary.roundsNumber;
+  document.querySelector("p.rounds-number span").textContent = gameSummary.roundsNumber;
   console.log(result);
-  if (result === 'win') {
+  if (result === "win") {
     gameSummary.playerScore++;
-    document.querySelector('p.player__score span').textContent = gameSummary.playerScore;
-  } else if (result === 'loss') {
+    document.querySelector("p.player__score span").textContent = gameSummary.playerScore;
+  } else if (result === "loss") {
     gameSummary.aiScore++;
-    document.querySelector('p.ai__score span').textContent = gameSummary.aiScore;
+    document.querySelector("p.ai__score span").textContent = gameSummary.aiScore;
   } else {
     gameSummary.ties++;
-    document.querySelector('p.ties span').textContent = gameSummary.ties;
+    document.querySelector("p.ties span").textContent = gameSummary.ties;
   }
 }
 
 function userChoice() {
-  const showPlayerChoice = document.querySelector(".player-choice");
   const playerChoice = this.cloneNode(true);
   showPlayerChoice.innerHTML = "";
   showPlayerChoice.appendChild(playerChoice);
@@ -77,6 +75,28 @@ function userChoice() {
   publishResult(game.playerHand, game.aiHand, gameResult)
 }
 
+function resetGame() {
+  let gameSummary = {
+    roundsNumber: 0,
+    playerScore: 0,
+    aiScore: 0,
+    ties: 0,
+  };
 
+  let game = {
+    playerHand: "",
+    aiHand: "",
+  };
+
+  document.querySelector("p.rounds-number span").textContent = gameSummary.roundsNumber;
+  document.querySelector("p.player__score span").textContent = gameSummary.playerScore;
+  document.querySelector("p.ai__score span").textContent = gameSummary.aiScore;
+  document.querySelector("p.ties span").textContent = gameSummary.ties;
+
+  showPlayerChoice.innerHTML = "";
+  showAiChoice.innerHTML = "";
+  roundResult.innerHTML = "";
+}
 
 hands.forEach((item) => item.addEventListener("click", userChoice));
+restartBtn.addEventListener("click", resetGame);
